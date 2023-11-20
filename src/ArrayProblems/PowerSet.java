30b35adeeb2765e746e5119b0f42a69228d2e758
Sam Arora <samarora1051@gmail.com>
2023-11-21 01:34:22 +0530

    MISC: Added PowerSet and DistinctInteger

---
+++ src/ArrayProblems/PowerSet.java
@@ -0,0 +1,38 @@
+package ArrayProblems;
+
+import java.util.*;
+
+public class PowerSet {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> input = new ArrayList<>();
+        input.add(1);
+        input.add(2);
+        input.add(3);
+
+        ArrayList<ArrayList<Integer>> powerSet = pwset(input);
+
+        System.out.println(powerSet);
+    }
+
+    public static ArrayList<ArrayList<Integer>> pwset(ArrayList<Integer> arr) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        generatePowerSet(arr, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void generatePowerSet(ArrayList<Integer> arr, int index, ArrayList<Integer> currentSet, ArrayList<ArrayList<Integer>> result) {
+        if (index == arr.size()) {
+            result.add(new ArrayList<>(currentSet));
+            return;
+        }
+
+        // Include the current element
+        currentSet.add(arr.get(index));
+        generatePowerSet(arr, index + 1, currentSet, result);
+
+        // Exclude the current element
+        currentSet.remove(currentSet.size() - 1);
+        generatePowerSet(arr, index + 1, currentSet, result);
+    }
+}
---
+++ src/ArrayProblems/SubarrayDistinctIntegers.java
@@ -0,0 +1,41 @@
+package ArrayProblems;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SubarrayDistinctIntegers {
+
+    public static int goodSubarrays(int arr[], int n,int b) {
+        if(n == 1 && b == 1) {
+            return 1;
+        }
+        int count = 0;
+        int slidingWindow = b;
+        while(slidingWindow<=n) {
+            for(int first = 0; first < n; first++) {
+                int last = first + slidingWindow;
+                if(last <= n) {
+                    count += checkDistinctNo(arr, first, last, b) ? 1: 0;
+                }
+            }
+            slidingWindow++;
+        }
+        return count;
+
+    }
+
+    static boolean checkDistinctNo(int[] arr, int f, int last, int b) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int j=f; j<last;j++) {
+            set.add(arr[j]);
+        }
+
+        boolean result = set.size() == b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(goodSubarrays(new int[]{1, 2, 3, 4, 5, 6}, 6, 6));
+    }
+}
