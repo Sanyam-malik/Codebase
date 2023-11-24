7a3d6f892a94730217270e3383511d29a17c49f1
Sam Arora <samarora1051@gmail.com>
2023-11-24 13:55:21 +0530

    Added RadixSort & LargestPerimeterTraingle Solutions

---
+++ src/ArrayProblems/LargestPerimeterTraingle.java
@@ -0,0 +1,23 @@
+package ArrayProblems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LargestPerimeterTraingle {
+        public static int maxPerimeterTriangle(ArrayList<Integer> arr, int n) {
+            Collections.sort(arr, Collections.reverseOrder());
+
+            int maxi = 0;
+            for(int i=0; i< n-2; i++) {
+                if(arr.get(i) < arr.get(i+1)+arr.get(i+2)) {
+                    maxi = Math.max(maxi, arr.get(i) + arr.get(i+1)+arr.get(i+2));
+                }
+            }
+            return maxi;
+        }
+
+    public static void main(String[] a) {
+        System.out.println(maxPerimeterTriangle(new ArrayList<>(List.of(2, 2, 3)), 3));
+    }
+}
