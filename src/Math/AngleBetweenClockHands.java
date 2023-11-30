e67452ab5a0f8ab4601f1a269c3000a17976c26f
Sam Arora <samarora1051@gmail.com>
2023-11-30 14:33:29 +0530

    MISC: Updated Code

--- src/Math/AngleBetweenClockHands.java
+++ src/Math/AngleBetweenClockHands.java
@@ -2,18 +2,35 @@ package Math;
 
 /*
  * <metadata>
- *   Status: InComplete,
- *   URL: https://www.codingninjas.com/studio/problems/angle-between-hour-hand-and-minute-hand_1062726,
- *   Notes: Test cases failing
+ *   Status: Completed,
+ *   URL: https://www.codingninjas.com/studio/problems/angle-between-hour-hand-and-minute-hand_1062726
  * </metadata>
  * */
 public class AngleBetweenClockHands {
 
+    public static double absoluteDiff(double a, double b) {
+        if(a > b) {
+            return a - b;
+        }
+        else{
+            return b - a;
+        }
+    }
     public static int findAngle(int hour, int minute) {
-        int angle= (int) Math.abs(30*hour+0.5*minute-6*minute);
+        if (hour == 12) {
+            hour = 0;
+        }
 
-        // Ensure the smaller angle is considered
-        return Math.min(angle, 360 - angle);
+        double hourAngle = 0.5 * (hour * 60 + minute);
+        double minuteAngle = 6 * (minute);
+
+        //Finding the angle with respect to the absolute difference.
+        double angle = absoluteDiff(hourAngle, minuteAngle);
+
+        //If the angle is more than 180 degrees we take the angle other way around.
+        angle = Math.min(angle, 360 - angle);
+
+        return (int)angle;
     }
 
     public static void main(String[] args) {
