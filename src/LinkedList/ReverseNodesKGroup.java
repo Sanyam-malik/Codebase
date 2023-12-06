a1ce477674146cff770d56db8a26e598d457ed5c
Sam Arora <samarora1051@gmail.com>
2023-12-07 04:44:48 +0530

    MISC: Added Question Placeholder

---
+++ src/LinkedList/ReverseNodesKGroup.java
@@ -0,0 +1,28 @@
+package LinkedList;
+
+/*
+ * <metadata>
+ *   Status:- InProgress,
+ *   URL:- https://www.codingninjas.com/studio/problems/reverse-blocks_763406,
+ *   Date:- 2023-12-07
+ * </metadata>
+ * */
+public class ReverseNodesKGroup {
+
+    static class Node
+    {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node getListAfterReverseOperation(Node head, int n, int b[]) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
