006a9c0e4f381f918f1efad96f31144f268da8f4
Sam Arora <samarora1051@gmail.com>
2023-12-12 15:53:02 +0530

    MISC: Added Placeholder for Asteriod Collision

---
+++ src/Problem_Of_The_Day/AsteriodCollision.java
@@ -0,0 +1,28 @@
+package Problem_Of_The_Day;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * <metadata>
+ *   Status:- InProgress,
+ *   URL:- https://www.codingninjas.com/studio/problems/asteroid-collision_977232
+ * </metadata>
+ * */
+public class AsteriodCollision {
+
+    public static List< Integer > collidingAsteroids(int []asteroids) {
+        List<Integer> res = new ArrayList<>();
+        int start = asteroids[0];
+        for(int i = start+1; i< asteroids.length; i++){
+
+        }
+
+        return res;
+    }
+    public static void main(String[] a) {
+        System.out.println(collidingAsteroids(new int[]{3, -2, 4}));
+    }
+
+
+}
