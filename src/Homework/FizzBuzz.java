320b85f9873e58b6365f0367af3b4b2b30ae03a5
Sam Arora <samarora1051@gmail.com>
2024-02-14 23:28:47 +0530

    Added Homework Problems

---
+++ src/Homework/FizzBuzz.java
@@ -0,0 +1,32 @@
+package Homework;
+
+/*
+ * <metadata>
+ *   Name:- FizzBuzz,
+ *   Status:- Completed,
+ *   URL:- https://www.hackerrank.com/challenges/fizzbuzz/problem,
+ *   Date:- 2024-02-14,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class FizzBuzz {
+
+    static void printFizzBuzz() {
+        for(int i=1; i< 100; i++) {
+            if(i%3 == 0 && i%5 ==0) {
+                System.out.println("FizzBuzz");
+            } else if(i%3==0) {
+                System.out.println("Fizz");
+            } else if(i%5==0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+    public static void main(String args[]) {
+        printFizzBuzz();
+    }
+
+}
---
+++ src/Homework/GCDTwoNumbers.java
@@ -0,0 +1,27 @@
+package Homework;
+
+/*
+ * <metadata>
+ *   Name:- GCD of two numbers,
+ *   Status:- Completed,
+ *   URL:- https://www.geeksforgeeks.org/problems/gcd-of-two-numbers3459,
+ *   Date:- 2024-02-14,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class GCDTwoNumbers {
+
+    public static int gcd(int A , int B) {
+        int gcd = 1;
+        int value = Math.min(A, B);
+        for(int i=2; i<= value; i++) {
+            if(A%i==0 && B%i==0) gcd = i;
+        }
+        return gcd;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(gcd(1, 1));
+    }
+}
