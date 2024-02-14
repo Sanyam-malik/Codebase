f4ec7b535ca7cb20336cb98a1f0ce2c2f1db3fa0
Sam Arora <samarora1051@gmail.com>
2024-02-14 23:08:25 +0530

    Added Homework Problems

---
+++ src/Homework/CommonFactors.java
@@ -0,0 +1,30 @@
+package Homework;
+
+/*
+ * <metadata>
+ *   Name:- Number of Common Factors,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/number-of-common-factors,
+ *   Date:- 2024-02-14,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class CommonFactors {
+
+    public static int commonFactors(int a, int b) {
+        int sum = 0;
+        int min = Math.min(a, b);
+        for(int i=1; i<=min; i++) {
+            if((a%i) == 0 && b%i==0){
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(commonFactors(25, 30));
+    }
+
+}
