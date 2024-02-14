497d592f727d039b3a773e1df1073002967d4f0e
Sam Arora <samarora1051@gmail.com>
2024-02-14 23:56:30 +0530

    Added Homework Problems

---
+++ src/Homework/PowerOfTwo.java
@@ -0,0 +1,31 @@
+package Homework;
+
+/*
+ * <metadata>
+ *   Name:- Power of Two,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/power-of-two,
+ *   Date:- 2024-02-14,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class PowerOfTwo {
+
+    /*
+    * Althrough It works in real mathematics it will not work in java due to inaccuracy of precision
+    * */
+    public static boolean isPowerOfTwo(int n) {
+        double temp = (Math.log(n)/Math.log(2));
+        System.out.println(temp);
+        return temp % 1 == 0;
+    }
+
+    public static boolean isPowerOfTwoBit(int n) {
+        return (n & (n-1)) == 0;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(isPowerOfTwoBit(536870912));
+    }
+}
