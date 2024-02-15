dc06a481d3f5be7623135a23029fc308a6a8328e
Sam Arora <samarora1051@gmail.com>
2024-02-15 17:28:28 +0530

    MISC: Added More Homework Solutions

---
+++ src/Homework/ArmstrongNumber.java
@@ -0,0 +1,21 @@
+package Homework;
+
+public class ArmstrongNumber {
+    static String armstrongNumber(int n){
+        int digits = (int) (Math.log10(n)+1);
+        char[] str = Integer.toString(n).toCharArray();
+        int sum = 0;
+        for(char ch: str) {
+            sum+= (int) Math.pow(Character.getNumericValue(ch), digits);
+        }
+
+        if(sum == n) {
+            return "Yes";
+        }
+        return "No";
+    }
+
+    public static void main(String args[]) {
+        System.out.println(armstrongNumber(372));
+    }
+}
---
+++ src/Homework/SumMultiples.java
@@ -0,0 +1,25 @@
+package Homework;
+
+/*
+ * <metadata>
+ *   Name:- Sum Multiples,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/sum-multiples,
+ *   Date:- 2024-02-15,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class SumMultiples {
+    public static int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i=1; i<=n; i++) {
+            if(i% 3 == 0 || i% 5 == 0 || i% 7 == 0) sum+=i;
+        }
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(sumOfMultiples(7));
+    }
+}
