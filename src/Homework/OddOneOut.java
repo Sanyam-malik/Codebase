089d73f8937af56911279e6db566fcb0e70ab264
Sam Arora <samarora1051@gmail.com>
2024-02-15 16:40:20 +0530

    MISc: Added HomeWork Problems

---
+++ src/Homework/OddOneOut.java
@@ -0,0 +1,28 @@
+package Homework;
+
+/*
+ * <metadata>
+ *   Name:- Odd One Out,
+ *   Status:- Completed,
+ *   URL:- https://codeforces.com/contest/1915/problem/A,
+ *   Date:- 2024-02-15,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class OddOneOut {
+
+    public static int oddOneOut(int a, int b, int c) {
+        if( a == b) {
+            return c;
+        } else if(b == c) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println(oddOneOut(1, 1, 2));
+    }
+}
---
+++ src/Homework/WaterConsumption.java
@@ -0,0 +1,22 @@
+package Homework;
+
+/*
+ * <metadata>
+ *   Name:- Water Consumption,
+ *   Status:- Completed,
+ *   URL:- https://www.codechef.com/problems/WATERCONS,
+ *   Date:- 2024-02-15,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class WaterConsumption {
+
+    public static String waterConsumption(int n) {
+        return n >=2000 ? "YES" : "NO";
+    }
+
+    public static void main(String args[]) {
+        System.out.println(waterConsumption(2000));
+    }
+}
