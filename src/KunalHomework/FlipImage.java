5a6125eb49e294803a93ef83eb1ea66cc7237ee0
Sam Arora <samarora1051@gmail.com>
2024-02-20 15:10:52 +0530

    Added Kunal Homework Problems

---
+++ src/KunalHomework/FlipImage.java
@@ -0,0 +1,38 @@
+package KunalHomework;
+
+
+/*
+ * <metadata>
+ *   Name:- Flipping an Image,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/flipping-an-image/,
+ *   Date:- 2024-02-18,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class FlipImage {
+
+    public int[][] flipAndInvertImage(int[][] image) {
+        // Mirror Flip
+        for(int r=0; r < image.length; r++) {
+            int cs = 0;
+            int ce = image[r].length - 1;
+            while(cs < ce) {
+                int temp = image[r][cs];
+                image[r][cs] = image[r][ce];
+                image[r][ce] = temp;
+                cs++;ce--;
+            }
+        }
+
+        // Inverse
+        for(int r=0; r < image.length; r++) {
+            for(int c=0; c < image[r].length; c++) {
+                image[r][c] = image[r][c] == 1 ? 0 : 1;
+            }
+        }
+
+        return image;
+    }
+}
