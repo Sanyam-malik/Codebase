228b2d7046d6980dac0fa979140bcf6287cd07f6
Sam Arora <samarora1051@gmail.com>
2024-02-20 13:50:14 +0530

    Added Kunal Homework Problems

---
+++ src/KunalHomework/CountItemsMatch.java
@@ -0,0 +1,24 @@
+package KunalHomework;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+ * <metadata>
+ *   Name:- Count Items Matching a Rule,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/count-items-matching-a-rule,
+ *   Date:- 2024-02-18,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class CountItemsMatch {
+
+    public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = ruleKey.equals("type") ? 0 : ruleKey.equals("color") ? 1 : 2;
+        items = items.stream().filter(x-> x.get(index).equals(ruleValue)).collect(Collectors.toList());
+        return items.size();
+    }
+
+}
---
+++ src/KunalHomework/HighAltitude.java
@@ -0,0 +1,27 @@
+package KunalHomework;
+
+
+
+/*
+ * <metadata>
+ *   Name:- Find the Highest Altitude,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/find-the-highest-altitude,
+ *   Date:- 2024-02-18,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class HighAltitude {
+
+    public static int largestAltitude(int[] gain) {
+        int runnningAltitude = 0;
+        int maxGain = 0;
+
+        for(int i=0; i< gain.length; i++) {
+            runnningAltitude+=gain[i];
+            maxGain = Math.max(maxGain, runnningAltitude);
+        }
+        return maxGain;
+    }
+}
