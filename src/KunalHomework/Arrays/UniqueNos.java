8bf76fd4f2e6c6d14193d0692f9c206063222035
Sam Arora <samarora1051@gmail.com>
2024-02-21 07:27:47 +0530

    Added TwoSum Solution

---
+++ src/KunalHomework/Arrays/UniqueNos.java
@@ -0,0 +1,33 @@
+package KunalHomework.Arrays;
+
+import java.util.Arrays;
+
+/*
+ * <metadata>
+ *   Name:- Find N Unique Integers Sum up to Zero,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero,
+ *   Date:- 2024-02-21,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class UniqueNos {
+
+    public static int[] sumZero(int n) {
+        int []ans = new int[n];
+        int left=0;
+        int right=n-1;
+        int start=1;
+            while(left<right) {
+                ans[left++]=start;
+                ans[right--]=-start;
+                start++;
+            }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(Arrays.toString(sumZero(5)));
+    }
+}
