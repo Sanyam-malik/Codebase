f7019ed17abe6265550aac40ddf7d6d3b06025da
Sam Arora <samarora1051@gmail.com>
2024-02-21 07:01:42 +0530

    Added MaximumPopulation Solution

---
+++ src/KunalHomework/Arrays/MaximumPopulation.java
@@ -0,0 +1,41 @@
+package KunalHomework.Arrays;
+
+/*
+ * <metadata>
+ *   Name:- Maximum Population Year,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/maximum-population-year,
+ *   Date:- 2024-02-20,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class MaximumPopulation {
+
+    public static int maximumPopulation(int[][] logs) {
+        int[] array = new int[101];
+        for(int[] log: logs) {
+            int end = log[1] - log[0];
+            int index = log[0] - 1950;
+            for(int start=0; start< end; start++) {
+                array[index] += 1;
+                index += 1;
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+        for(int i=0; i< array.length; i++) {
+            if(array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+
+        return index+1950;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(maximumPopulation(new int[][]{{1982,1998},{2013,2042},{2010,2035},{2022,2050},{2047,2048}}));
+    }
+}
