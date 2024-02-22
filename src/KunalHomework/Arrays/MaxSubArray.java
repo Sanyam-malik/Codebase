bc9647a3e40efc5e85bccd6b44b7bdbedcefa9aa
Sam Arora <samarora1051@gmail.com>
2024-02-22 13:44:52 +0530

    Added More Solutions

---
+++ src/KunalHomework/Arrays/MaxSubArray.java
@@ -0,0 +1,31 @@
+package KunalHomework.Arrays;
+
+
+
+/*
+ * <metadata>
+ *   Name:- Maximum Subarray,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/maximum-subarray,
+ *   Date:- 2024-02-12,
+ *   Level:- Medium,
+ *   Notes:- TLE Error,
+ * </metadata>
+ * */
+public class MaxSubArray {
+
+    public int maxSubArray(int[] nums) {
+        int runningSum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i< nums.length; i++) {
+            runningSum += nums[i];
+            max = Math.max(runningSum, max);
+            if(runningSum < 0) {
+                runningSum = 0;
+            }
+        }
+        return max;
+    }
+
+}
