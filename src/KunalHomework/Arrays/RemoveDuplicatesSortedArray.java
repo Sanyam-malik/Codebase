229523c0df7101f73e5dddd9168330959708c86b
Sam Arora <samarora1051@gmail.com>
2024-02-23 11:51:55 +0530

    Added More Solutions

---
+++ src/KunalHomework/Arrays/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,28 @@
+package KunalHomework.Arrays;
+
+
+
+/*
+ * <metadata>
+ *   Name:- Remove Duplicates from Sorted Array,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/remove-duplicates-from-sorted-array/,
+ *   Date:- 2024-02-23,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class RemoveDuplicatesSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+}
