e8141d414d95c2cd9c89c1dcfbdc08a8695eeb16
Sam Arora <samarora1051@gmail.com>
2024-02-27 21:26:52 +0530

    Added Plus One Solution

---
+++ src/Arrays/PlusOne.java
@@ -0,0 +1,58 @@
+package Arrays;
+
+import java.util.Arrays;
+
+/*
+ * <metadata>
+ *   Name:- Plus One,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/plus-one,
+ *   Date:- 2024-02-23,
+ *   Level:- Easy,
+ *   Notes:- No Notes Needed,
+ * </metadata>
+ * */
+public class PlusOne {
+
+    public static int[] plusOne(int[] digits) {
+        Integer[] revNo = reverseNo(digits);
+        int carry = 1;
+        int index = 0;
+        do {
+            int sum = revNo[index] != null ? revNo[index] + carry : carry;
+            revNo[index++] = sum % 10;
+
+            if(sum >= 10) {
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+        } while (carry > 0);
+        int size = digits.length;
+        if(revNo[revNo.length-1] != null) {
+            size+= 1;
+        }
+        int[] result = new int[size];
+        int indx = 0;
+        for(int i=revNo.length-1; i>=0; i--) {
+            if(revNo[i] != null) {
+                result[indx++] = revNo[i];
+            }
+        }
+
+        return result;
+    }
+
+    public static Integer[] reverseNo(int[] no){
+        Integer[] temp = new Integer[no.length+1];
+        int index = 0;
+        for(int i=no.length-1; i>=0; i--) {
+            temp[index++] = no[i];
+        }
+        return temp;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(Arrays.toString(plusOne(new int[]{4, 2, 9})));
+    }
+}
