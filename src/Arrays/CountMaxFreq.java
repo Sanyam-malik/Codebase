5a6db8d1a3fd42663c3b9d66985957baaedaa0c8
Sam Arora <samarora1051@gmail.com>
2024-03-05 01:49:31 +0530

    Revised this Question

--- src/Arrays/CountMaxFreq.java
+++ src/Arrays/CountMaxFreq.java
@@ -3,7 +3,7 @@ package Arrays;
 /*
  * <metadata>
  *   Name:- Count Elements With Maximum Frequency,
- *   Description: You are given an array nums consisting of positive integers.</br>
+ *   Description:- You are given an array nums consisting of positive integers.</br>
         Return the total frequencies of elements in nums such that those elements all have the maximum frequency.</br>
         The frequency of an element is the number of occurrences of that element in the array.,
  *   Status:- Completed,
