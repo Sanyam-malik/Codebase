ccdafb3feda7de3295568c2f2b3c07f4d11f93e0
Sam Arora <samarora1051@gmail.com>
2024-03-05 01:51:02 +0530

    Revised this Question

--- src/Arrays/CountMaxFreq.java
+++ src/Arrays/CountMaxFreq.java
@@ -3,14 +3,14 @@ package Arrays;
 /*
  * <metadata>
  *   Name:- Count Elements With Maximum Frequency,
- *   Description:- You are given an array nums consisting of positive integers.</br>
-        Return the total frequencies of elements in nums such that those elements all have the maximum frequency.</br>
+ *   Description:- You are given an array nums consisting of positive integers.</br></br>
+        Return the total frequencies of elements in nums such that those elements all have the maximum frequency.</br></br>
         The frequency of an element is the number of occurrences of that element in the array.,
  *   Status:- Completed,
  *   URL:- https://leetcode.com/problems/count-elements-with-maximum-frequency,
  *   Date:- 2024-02-23,
  *   Level:- Easy,
- *   Notes:- Solution 1 Hint - Have Two Variables max and count and iterate over 2 times?</br>
+ *   Notes:- Solution 1 Hint - Have Two Variables max and count and iterate over 2 times?</br></br>
  *      Solution 2 Hint - Maybe Use Extra Space Probably Array or HashMap?,
  * </metadata>
  * */
