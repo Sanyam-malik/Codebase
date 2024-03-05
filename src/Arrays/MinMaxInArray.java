8c8ebbce6accd99a7513a2a95abcec79f9a2d020
Sam Arora <samarora1051@gmail.com>
2024-03-05 13:13:06 +0530

    Revised this Question

--- src/Arrays/MinMaxInArray.java
+++ src/Arrays/MinMaxInArray.java
@@ -6,10 +6,13 @@ import java.util.Arrays;
  * <metadata>
  *   Name:- Find minimum and maximum element in an array,
  *   Status:- Completed,
- *   URL:- https://www.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428,
+ *   Description:- Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.</br></br>
+ *      <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdCBmcm9tIDIwMjQtMDMtMDUgMTMtMTItMDQucG5n&version_id=null">
+ *   URL:- https://www.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1,
  *   Date:- 2024-02-23,
  *   Level:- Easy,
- *   Notes:- No Notes Needed,
+ *   Notes:- Solution 1 - Two Four Loops for Min and Max or convert into single one O(n)?</br></br>
+ *           Solution 2 - Sort Ascending O(n log n)?,
  * </metadata>
  * */
 public class MinMaxInArray {
