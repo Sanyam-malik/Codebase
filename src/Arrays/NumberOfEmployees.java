f3d7d0f1a73a62b53b7776c1fcc182ad5cf11306
Sam Arora <samarora1051@gmail.com>
2024-03-05 19:05:49 +0530

    Added Missing Metadata

--- src/Arrays/NumberOfEmployees.java
+++ src/Arrays/NumberOfEmployees.java
@@ -3,11 +3,12 @@ package Arrays;
 /*
  * <metadata>
  *   Name:- Number of Employees Who Met the Target,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdCBmcm9tIDIwMjQtMDMtMDUgMTktMDQtMzgucG5n&version_id=null">,
  *   Status:- Completed,
  *   URL:- https://leetcode.com/problems/number-of-employees-who-met-the-target,
  *   Date:- 2024-02-23,
  *   Level:- Easy,
- *   Notes:- No Notes Needed,
+ *   Notes:- Simple Compare is Enough,
  * </metadata>
  * */
 public class NumberOfEmployees {
