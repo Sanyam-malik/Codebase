552ae830534968afc816094e572370c79d74e0f1
Sam Arora <samarora1051@gmail.com>
2024-03-05 14:25:39 +0530

    Revised this Question

--- src/Arrays/MaxConsecutiveOnes.java
+++ src/Arrays/MaxConsecutiveOnes.java
@@ -3,10 +3,11 @@ package Arrays;
  * <metadata>
  *   Name:- Max Consecutive Ones,
  *   Status:- Completed,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdCBmcm9tIDIwMjQtMDMtMDUgMTQtMjMtNDgucG5n&version_id=null">,
  *   URL:- https://leetcode.com/problems/max-consecutive-ones,
  *   Date:- 2024-02-12,
  *   Level:- Easy,
- *   Notes:- No Notes,
+ *   Notes:- Solution - Have two variables max and count when number is changed compare max with count and save new max and make count = 0,
  * </metadata>
  * */
 public class MaxConsecutiveOnes {
