f8878f5520b04d1c1ec6e314d04984c784ef28a5
Sam Arora <samarora1051@gmail.com>
2024-03-05 19:00:43 +0530

    Added Missing Metadata

--- src/Arrays/MinNumberGame.java
+++ src/Arrays/MinNumberGame.java
@@ -4,11 +4,12 @@ import java.util.*;
 /*
  * <metadata>
  *   Name:- Minimum Number Game,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdCBmcm9tIDIwMjQtMDMtMDUgMTgtNTgtMDMucG5n&version_id=null">,
  *   Status:- Completed,
  *   URL:- https://leetcode.com/problems/minimum-number-game,
  *   Date:- 2024-02-23,
  *   Level:- Easy,
- *   Notes:- No Notes Needed,
+ *   Notes:- Sort the array or find the minimum each time until array length reaches then append bob value and then alice value into a new array.,
  * </metadata>
  * */
 public class MinNumberGame {
