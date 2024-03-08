8df546aabe613daae4ca7c06562be4e811c663da
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-08 14:23:33 +0530

    Added LeftRightSumDiff

---
+++ src/Arrays/LeftRightSumDiff.java
@@ -0,0 +1,44 @@
+package Arrays;
+
+
+
+/*
+ * <metadata>
+ *   Name:- Left and Right Sum Differences,
+ *   Description:-
+        <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdF8xNS5wbmc=&version_id=null">,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/left-and-right-sum-differences/description/,
+ *   Date:- 2024-03-08,
+ *   Level:- Easy,
+ *   Notes:- Three Arrays two for saving running sums and one to save the result,
+ * </metadata>
+ * */
+public class LeftRightSumDiff {
+
+    public int[] leftRightDifference(int[] nums) {
+
+        int[] leftSum = new int[nums.length];
+        int[] rightSum = new int[nums.length];
+
+        int leftRunningSum = 0;
+        for(int i=0; i< nums.length; i++) {
+            leftSum[i] = leftRunningSum;
+            leftRunningSum += nums[i];
+        }
+
+        int rightRunningSum = 0;
+        for(int i=nums.length-1; i>=0; i--) {
+            rightSum[i] = rightRunningSum;
+            rightRunningSum += nums[i];
+        }
+
+        int[] ans = new int[nums.length];
+        for(int i=0; i< ans.length; i++) {
+            ans[i] = (int)Math.abs(leftSum[i]-rightSum[i]);
+        }
+
+        return ans;
+    }
+
+}
