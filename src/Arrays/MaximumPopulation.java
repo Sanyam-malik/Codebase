2903d6b8f9ad8e2bfde943255c400c8a1b355c6f
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-09 00:40:23 +0530

    Added MaximumPopulation

---
+++ src/Arrays/MaximumPopulation.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+/*
+ * <metadata>
+ *   Name:- Maximum Population Year,
+ *   Description:-
+        <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdF8yMC5wbmc=&version_id=null">,
+ *   Status:- Completed - Review Again,
+ *   URL:- https://leetcode.com/problems/maximum-population-year/description/,
+ *   Date:- 2024-03-08,
+ *   Level:- Easy,
+ *   Notes:-
+ *           Solution 1 Hint - Create a array of 100 length and add the 1 for range and then find the max,
+ * </metadata>
+ * */
+public class MaximumPopulation {
+
+    public int maximumPopulation(int[][] logs) {
+        int[] array = new int[101];
+        for(int[] log: logs) {
+            int end = log[1] - log[0];
+            int index = log[0] - 1950;
+            for(int start=0; start< end; start++) {
+                array[index++] += 1;
+            }
+        }
+
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+        for(int i=0; i< array.length; i++) {
+            if(array[i] > max) {
+                max = array[i];
+                index = i;
+            }
+        }
+
+        return index+1950;
+    }
+
+}
