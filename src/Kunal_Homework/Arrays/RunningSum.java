c92783ea21845ad740fb772b4dea7ca3e56122a2
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-10 23:34:23 +0530

    Added Metadata

--- src/Kunal_Homework/Arrays/RunningSum.java
+++ src/Kunal_Homework/Arrays/RunningSum.java
@@ -3,11 +3,13 @@ package Kunal_Homework.Arrays;
 /*
  * <metadata>
  *   Name:- Running Sum of 1d Array,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdF82Ni5wbmc=&version_id=null">,
  *   Status:- Completed,
  *   URL:- https://leetcode.com/problems/running-sum-of-1d-array,
  *   Date:- 2024-02-18,
  *   Level:- Easy,
  *   Notes:- No Notes Needed,
+ *   Companies:- Microsoft,
  * </metadata>
  * */
 public class RunningSum {
