c03a2a233dc98badd1342e8b640997728fe2a110
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-10 05:44:58 +0530

    Added Metadata

--- src/Kunal_Homework/Arrays/BuildArray.java
+++ src/Kunal_Homework/Arrays/BuildArray.java
@@ -4,10 +4,12 @@ package Kunal_Homework.Arrays;
 /*
  * <metadata>
  *   Name:- Build Array from Permutation,
- *   Status:- Needs Improvement,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdF80Ny5wbmc=&version_id=null">,
+ *   Status:- Incomplete?,
  *   URL:- https://leetcode.com/problems/build-array-from-permutation,
  *   Date:- 2024-02-18,
  *   Level:- Easy,
+ *   Companies:- Uber:Apple:Adobe:Google,
  *   Notes:- No Notes Needed,
  * </metadata>
  * */
