b4c8099bb73f40a64caf38620dac424c72bad191
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-11 23:22:35 +0530

    Added Metadata

--- src/Arrays/CountNiceSubArrays.java
+++ src/Arrays/CountNiceSubArrays.java
@@ -11,16 +11,32 @@ package Arrays;
  *   Level:- Medium,
  *   Remarks:- Needs Practice Related to This Problem,
  *   Notes:- Solution 1- Using list and two for loops</br></br>
- *      Solution 2- </br><iframe width="560" height="315" src="https://www.youtube.com/embed/atUJS7ArOY0?si=cez2IYUKJf0O1DNe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
+ *      Solution 2- </br><iframe width="560" height="315" src="https://www.youtube.com/embed/atUJS7ArOY0?si=cez2IYUKJf0O1DNe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></br>
+ *      Solution 3- </br><iframe width="560" height="315" src="https://www.youtube.com/embed/zm460v4n2Ic?si=xyJq-UZ6Z_1Ct5CT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>,
  * </metadata>
  * */
 public class CountNiceSubArrays {
     public static int numberOfSubarrays(int[] nums, int k) {
         int result = 0;
-
-        for(int i=1; i< nums.length;i++) {
-
+        // convert to subarray with sum equal to k
+        int[] sub = new int[nums.length];
+        for(int i=0; i< nums.length;i++) {
+            if(nums[i] % 2==0) {
+                sub[i]=0;
+            } else {
+                sub[i]=1;
+            }
         }
+
+        // saving running sums
+        int[] res = new int[sub.length];
+        res[0] = sub[0];
+        for(int i=1; i< sub.length;i++) {
+            res[i] += res[i-1];
+        }
+
+        for()
+
         return result;
     }
 
