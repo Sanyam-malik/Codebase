77a5d12987600031cae048c108d43a4aadb2feae
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-11 16:27:42 +0530

    Added Metadata

---
+++ src/Arrays/CountNiceSubArrays.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+
+/*
+ * <metadata>
+ *   Name:- Count Number of Nice Subarrays,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdF83My5wbmc=&version_id=null">,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/count-number-of-nice-subarrays/description,
+ *   Date:- 2024-03-11,
+ *   Level:- Medium,
+ *   Remarks:- Needs Practice Related to This Problem,
+ *   Notes:- Solution 1- Using list and two for loops,
+ * </metadata>
+ * */
+public class CountNiceSubArrays {
+    public static int numberOfSubarrays(int[] nums, int k) {
+        int result = 0;
+
+        for(int i=1; i< nums.length;i++) {
+
+        }
+        return result;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(numberOfSubarrays(new int[]{1,1,2,1,1}, 1));
+    }
+}
