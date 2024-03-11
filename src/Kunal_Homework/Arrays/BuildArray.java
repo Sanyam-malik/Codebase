6173114fbf5c64d4018df618b911f87cb6d8e537
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-11 15:21:42 +0530

    Added Metadata

--- src/Kunal_Homework/Arrays/BuildArray.java
+++ src/Kunal_Homework/Arrays/BuildArray.java
@@ -5,7 +5,7 @@ package Kunal_Homework.Arrays;
  * <metadata>
  *   Name:- Build Array from Permutation,
  *   Description:- <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdF80Ny5wbmc=&version_id=null">,
- *   Status:- Incomplete?,
+ *   Status:- Completed,
  *   URL:- https://leetcode.com/problems/build-array-from-permutation,
  *   Date:- 2024-02-18,
  *   Level:- Easy,
