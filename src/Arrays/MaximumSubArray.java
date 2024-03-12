92e4b4bf1757e85fdc5614798f19ef76a61da6e2
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-12 14:57:26 +0530

    Added Metadata

---
+++ src/Arrays/MaximumSubArray.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+/*
+ * <metadata>
+ *   Name:- Maximum Subarray,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9001/api/v1/buckets/codebase/objects/download?preview=true&prefix=U2NyZWVuc2hvdF83NS5wbmc=&version_id=null">,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/maximum-subarray/description/,
+ *   Date:- 2024-03-12,
+ *   Level:- Medium,
+ *   Remarks:- Solve Using Divide and Conquer:LearnYard Problem,
+ *   Notes:- Solution - Use Kadane's Algorithm,
+ * </metadata>
+ * */
+public class MaximumSubArray {
+
+    public int maxSubArray(int[] nums) {
+        int runningSum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i< nums.length; i++) {
+            runningSum += nums[i];
+            max = Math.max(runningSum, max);
+            if(runningSum < 0) {
+                runningSum = 0;
+            }
+        }
+        return max;
+    }
+
+}
