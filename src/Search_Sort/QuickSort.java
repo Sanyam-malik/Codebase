5aa7762737d900c126fdd892d6d1fa47842703b4
Sanyam Malik <sanyam.malik@clayhr.com>
2024-03-18 13:44:46 +0530

    Added QuickSort

---
+++ src/Search_Sort/QuickSort.java
@@ -0,0 +1,15 @@
+package Search_Sort;
+
+/*
+ * <metadata>
+ *   Name:- Quick Sort,
+ *   Status:- InComplete,
+ *   URL:- https://wemakedevs.org,
+ *   Date:- 2024-03-18,
+ *   Level:- Easy,
+ *   Notes:- Select the pivot, divide the array and sort according to pivot at each interval,
+ *   Remarks:- Important Technique:LearnYard Problem,
+ * </metadata>
+ * */
+public class QuickSort {
+}
