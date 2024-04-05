fb665a23093d8c8d900820e139f2a391aaa11373
Sam Arora <samarora1051@gmail.com>
2024-04-05 15:20:27 +0530

    Added MinimumDiffBwPairs

---
+++ src/Sorting/MinimumDiffBwPairs.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+import java.util.Arrays;
+
+/*
+ * <metadata>
+ *   Name:- Minimum difference between the pairs of elements,
+ *   Description:- Given an array arr find minimum pair of elements such that i != j,
+ *   Status:- Completed,
+ *   URL:- https://learnyard.com,
+ *   Date:- 2024-04-04,
+ *   Level:- Easy,
+ *   Notes:- Just Sort and find the minimum of difference,
+ *   Remarks:- LearnYard Problem,
+ * </metadata>
+ * */
+public class MinimumDiffBwPairs {
+
+    static int minimumDifference(int arr[]) {
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i< arr.length-1; i++) {
+            min = Math.min(min, Math.abs(arr[i+1] - arr[i]));
+        }
+        return min;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(minimumDifference(new int[]{1, 4, 6, 7, 8, 10}));
+    }
+}
