94b3263d435c1e5715747731648812961c3ad7b8
Sam Arora <samarora1051@gmail.com>
2024-04-05 13:36:53 +0530

    Added NutsBolts.java

---
+++ src/Sorting/NutsBolts.java
@@ -0,0 +1,21 @@
+package Sorting;
+import java.util.*;
+
+/*
+ * <metadata>
+ *   Name:- Nuts Bolts,
+ *   Description: <img src="http://lordmaximus.duckdns.org:9000/codebase/Nuts_bolts.png">,
+ *   Status:- Completed,
+ *   URL:- https://wemakedevs.org,
+ *   Date:- 2024-03-18,
+ *   Level:- Easy,
+ *   Notes:- Just Sort,
+ *   Remarks:- LearnYard Problem,
+ * </metadata>
+ * */
+public class NutsBolts {
+    void matchPairs(char nuts[], char bolts[], int n) {
+        Arrays.sort(nuts);
+        Arrays.sort(bolts);
+    }
+}
