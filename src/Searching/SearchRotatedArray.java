a27996256b85c6792f30e0168b0595f5b6ede0a0
Sam Arora <samarora1051@gmail.com>
2024-04-10 14:03:54 +0530

    Updated Metadata

--- src/Searching/SearchRotatedArray.java
+++ src/Searching/SearchRotatedArray.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
  *   Level:- Medium,
  *   Notes:- <iframe width="560" height="315" src="https://www.youtube.com/embed/5qGrJbHhqFs?si=KOS3nkO0xf67nwck" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> <br /> <iframe width="560" height="315" src="https://www.youtube.com/embed/hywGbVJldj0?si=cK0QrsxoXi5W2s4L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>,
  *   Companies:- tcs:OYO:Hike:Grab:Visa:Paytm:Optum:Paytm:Park+:Cisco:Apple:Eaton:Adobe:Yahoo:Amazon:Intuit:Spinny:PayPal:Redbus:Qualys:InMobi:Practo:Maersk:Airtel:Twitch:Yandex:Zillow:Zulily:Oracle:tiktok:VMware:Google:Nvidia:Splunk:DE Shaw:FactSet:Siemens:PhonePe:Proteum:Expedia:Curefit:Groupon:Samsung:Sigmoid:Nagarro:Alibaba:Tencent:Expedia:Flipkart:Snapdeal:Facebook:Qualcomm:Arcesium:Travclan:JPMorgan:SAP Labs:LinkedIn:Rippling:MindTree:HashedIn:Citicorp:Microsoft:Cognizant:Innovacer:Capgemini:Accenture:ShareChat:Delhivery:Bloomberg:Traveloka:ByteDance:MakeMyTrip:BankBazaar:Freecharge:Freshworks:ServiceNow:Salesforce:Informatica:ION Trading:Prystin Care:ZS Associates:Interics Tech:Goldman Sachs:Times Internet:Morgan Stanley:Disney+ Hotstar:NCR Corporation:Kempston Global:EagleView India:Vimana Aerotech:American Express:Navi Technologies:Walmart Global Tech:Veridic Private Limited,
- *   Remarks:- LearnYard Problem:Good Problem,
+ *   Remarks:- LearnYard Problem:Good Question,
  * </metadata>
  * */
 public class SearchRotatedArray {
