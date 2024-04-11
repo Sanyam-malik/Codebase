93a563f098b8d22f93a0ef207acbfeafd9e51517
Sam Arora <samarora1051@gmail.com>
2024-04-12 03:20:06 +0530

    Added MinimumPlatforms

--- src/Sorting/MinimumPlatforms.java
+++ src/Sorting/MinimumPlatforms.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  *   URL:- https://www.geeksforgeeks.org/problems/minimum-platforms-1587115620/1,
  *   Date:- 2024-04-12,
  *   Level:- Medium,
- *   Notes:- Just Sort and while arr[i] <= dept[i] platform++ else platform-- </br> <a href="https://takeuforward.org/data-structure/minimum-number-of-platforms-required-for-a-railway/">Solution Link</a>,
+ *   Notes:- Just Sort and while arr[i] <= dept[i] platform++ else platform-- </br></br> <a href="https://takeuforward.org/data-structure/minimum-number-of-platforms-required-for-a-railway/">Solution Link</a> </br></br> <iframe width="560" height="315" src="https://www.youtube.com/embed/dxVcMDI7vyI?si=RFVQOlkLpKeTXcKF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>,
  *   Companies:- Paytm:Amazon:Microsoft:DE Shaw:Hike:Walmart:Adobe:Google:Boomerang Commerce:Zillious:Atlassian,
  *   Remarks:- LearnYard Problem,
  * </metadata>
