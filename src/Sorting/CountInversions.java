d0dfa96605d800500f2e63bb269beeec87ec85f7
Sam Arora <samarora1051@gmail.com>
2024-04-11 15:17:36 +0530

    Added Count inversions

--- src/Sorting/CountInversions.java
+++ src/Sorting/CountInversions.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * <metadata>
  *   Name:- Count Inversions,
  *   Description:- <img src="http://lordmaximus.duckdns.org:9000/codebase/CountInversions.png">,
- *   Status:- Attempted,
+ *   Status:- Try Again,
  *   URL:- https://www.geeksforgeeks.org/problems/inversion-of-array-1587115620/1,
  *   Date:- 2024-04-11,
  *   Level:- Medium,
@@ -16,70 +16,47 @@ import java.util.Arrays;
  * </metadata>
  * */
 public class CountInversions {
+    static long conquer(long arr[],int lo,int mid,int hi){
+        long temp[]=new long[hi-lo+1];
+        int pos=0;
+        int i=lo;
+        int j=mid+1;
+        long invCount=0;
+        while(i<=mid && j<=hi){
+            if(arr[i]>arr[j]){
+                temp[pos++]=arr[j++];
+                invCount+=(mid-i+1);
+            }
+            else{
+                temp[pos++]=arr[i++];
+            }
+        }
 
-    static int inversions = 0;
+        while(i<=mid){
+            temp[pos++]=arr[i++];
+        }
+        while(j<=hi){
+            temp[pos++]=arr[j++];
+        }
+
+        for(int k=0,l=lo;k<temp.length;k++,l++){
+            arr[l]=temp[k];
+        }
+        return invCount;
+    }
+    static long divide(long arr[],int lo,int hi){
+        if(lo>=hi){
+            return 0;
+        }
+        int mid=lo+(hi-lo)/2;
+        long invCount=divide(arr,lo,mid);
+        invCount+=divide(arr,mid+1,hi);
+        invCount+=conquer(arr,lo,mid,hi);
+        return invCount;
+    }
     static long inversionCount(long arr[], long N) {
-        int sort = checkIfSortedInAnyOrder(arr);
-        // It is already sorted
-        if(sort == 0) return 0;
-        // It is descending sorted
-        else if(sort == 1) return N;
-        else {
-            sort(arr);
-        }
-        return inversions;
+        // Your Code Here
+        return divide(arr,0,(int)N-1);
     }
 
-    public static long[] sort(long[] arr){
-        int n = arr.length;
-        if(n == 1) return arr;
-        long[] arr1 = sort(Arrays.copyOfRange(arr, 0, n/2));
-        long[] arr2 = sort(Arrays.copyOfRange(arr, n/2, n));
-        return merge2Sorted(arr1, arr2);
-    }
-
-    public static long[] merge2Sorted(long[] arr1, long[] arr2) {
-        long[] result = new long[arr1.length+ arr2.length];
-        int index = 0;
-        int idx1 = 0;
-        int idx2 = 0;
-        while(idx1 < arr1.length && idx2 < arr2.length) {
-            if(arr1[idx1] <= arr2[idx2]) {
-                result[index++] = arr1[idx1++];
-            } else {
-                result[index++] = arr2[idx2++];
-                inversions++;
-            }
-        }
-
-        while (idx1 < arr1.length) {
-            result[index++] = arr1[idx1++];
-        }
-
-        while (idx2 < arr2.length) {
-            result[index++] = arr2[idx2++];
-        }
-
-        return result;
-    }
-
-    static int checkIfSortedInAnyOrder(long arr[]) {
-        int sort = -1;
-        for(int i=0; i< arr.length-1; i++) {
-            if(arr[i] < arr[i+1]) {
-                sort = 0;
-            } else {
-                sort = -1;
-            }
-        }
-
-        for(int i=0; i< arr.length-1; i++) {
-            if(arr[i] > arr[i+1]) {
-                sort = 1;
-            } else {
-                sort = -1;
-            }
-        }
-        return sort;
-    }
 }
