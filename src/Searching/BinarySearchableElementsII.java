b1ed43d1ab3c3ae1924b08d09f498185baa1e3da
Sam Arora <samarora1051@gmail.com>
2024-04-11 06:41:33 +0530

    Updated Metadata

--- src/Searching/BinarySearchableElementsII.java
+++ src/Searching/BinarySearchableElementsII.java
@@ -4,7 +4,7 @@ package Searching;
 /*
  * <metadata>
  *   Name:- Binary Searchable Elements II,
- *   Description:- Given an array [1 14 6 21 57 8]> try to find the elements that can be searched using binary search if middle is chosen randomly,
+ *   Description:- Given an array [1 14 6 21 57 8] try to find the elements that can be searched using binary search if middle is chosen randomly,
  *   Status:- Completed,
  *   URL:- https://learnyard.com,
  *   Date:- 2024-04-08,
