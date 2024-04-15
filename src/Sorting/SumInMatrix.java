64be6ef3bb3097305fc441722ae44f867a55ce75
Sam Arora <samarora1051@gmail.com>
2024-04-15 15:28:35 +0530

    Added SumInMatrix

---
+++ src/Sorting/SumInMatrix.java
@@ -0,0 +1,39 @@
+package Sorting;
+import java.util.*;
+
+
+
+/*
+ * <metadata>
+ *   Name:- https://leetcode.com/problems/sum-in-a-matrix/,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9000/codebase/SumInMatrix.png">,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/sum-in-a-matrix/description/,
+ *   Date:- 2024-04-15,
+ *   Level:- Medium,
+ *   Notes:- Just sort matrix rowwise and normal for loop to find maximum and adding it to score,
+ *   Remarks:- LearnYard Problem:Good Question,
+ * </metadata>
+ * */
+public class SumInMatrix {
+
+    public int matrixSum(int[][] nums) {
+        int score = 0;
+        int n = nums.length;
+        int m = nums[0].length;
+        for(int[] a :nums)
+        {
+            Arrays.sort(a);
+        }
+        for(int i=0;i<m;i++)
+        {
+            int max = 0;
+            for(int j=0;j<n;j++)
+            {
+                max = Math.max(max,nums[j][i]);
+            }
+            score+=max;
+        }
+        return score;
+    }
+}
