5ee4cb062b2d4a3e2a578285a30047ce90e809ab
Sam Arora <samarora1051@gmail.com>
2024-04-16 08:47:25 +0530

    Added ContainsDuplicate

---
+++ src/Sorting/ContainsDuplicate.java
@@ -0,0 +1,40 @@
+package Sorting;
+import java.util.*;
+
+/*
+ * <metadata>
+ *   Name:- Majority Element,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9000/codebase/ContainsDuplicate.png">,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/contains-duplicate/,
+ *   Date:- 2024-04-16,
+ *   Level:- Easy,
+ *   Notes:- 1. Using Arrays.sort() </br> 2. Using Sets </br>,
+ *   Companies:- TCS:Uber:Adobe:Apple:Yahoo:Amazon:Google:Microsoft:Bloomberg,
+ *   Remarks:- LearnYard Problem,
+ * </metadata>
+ * */
+public class ContainsDuplicate {
+
+    public boolean containsDuplicateSet(int[] nums) {
+        Set<Integer> numsSet = new HashSet<>();
+        for(int i = 0; i < nums.length ; i++){
+            if(numsSet.contains(nums[i])){
+                return true;
+            }
+            numsSet.add(nums[i]);
+        }
+        return false;
+    }
+
+    public boolean containsDuplicateSorting(int[] nums) {
+        Arrays.sort(nums);
+        int prevElem = nums[0];
+        for(int i=1; i<nums.length; i++) {
+            if(prevElem == nums[i]) return true;
+            prevElem = nums[i];
+        }
+        return false;
+    }
+
+}
