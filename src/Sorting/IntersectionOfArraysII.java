f507bbb0d7e3c2eea54a71db971dcd40d25330cd
Sam Arora <samarora1051@gmail.com>
2024-04-16 13:15:11 +0530

    Added IntersectionOfArraysII

---
+++ src/Sorting/IntersectionOfArraysII.java
@@ -0,0 +1,79 @@
+package Sorting;
+
+import java.util.*;
+
+/*
+ * <metadata>
+ *   Name:- Intersection of Two Arrays II,
+ *   Description:- <img src="http://lordmaximus.duckdns.org:9000/codebase/IntersectionBwArraysII.png">,
+ *   Status:- Completed,
+ *   URL:- https://leetcode.com/problems/intersection-of-two-arrays-ii/description/,
+ *   Date:- 2024-04-16,
+ *   Level:- Easy,
+ *   Notes:- 1. Use HashMap </br> 2. Use Arrays as Map,
+ *   Remarks:- Kunal Kushwaha,
+ *   Companies:- Yahoo:Adobe:Amazon:Facebook:Bloomberg,
+ * </metadata>
+ * */
+public class IntersectionOfArraysII {
+
+    public int[] intersectOptimisedToHell(int[] nums1, int[] nums2) {
+        int map[]=new int[1001];
+        int res[]=new int[1001];
+        int count=0;
+        for(int i:nums1) {
+            map[i]++;
+        }
+        for(int i:nums2){
+            if(map[i]>0){
+                res[count++]=i;
+                map[i]--;
+            }
+        }
+        return Arrays.copyOfRange(res,0,count);
+    }
+
+    public int[] intersectWithMapOptimised(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map1 = new HashMap<Integer, Integer>();
+        List<Integer> list = new ArrayList<>();
+
+        for(int i: nums1) {
+            map1.put(i, map1.getOrDefault(i, 0)+1);
+        }
+
+        for(int i: nums2) {
+            if(map1.containsKey(i) && map1.get(i) > 0) {
+                map1.put(i, map1.get(i)-1);
+                list.add(i);
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+    public int[] intersectWithMap(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map1 = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map2 = new HashMap<Integer, Integer>();
+
+        for(int i: nums1) {
+            map1.put(i, map1.getOrDefault(i, 0)+1);
+            map2.put(i, 0);
+        }
+
+        for(int i: nums2) {
+            map2.put(i, map2.getOrDefault(i, 0)+1);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(Integer key: map1.keySet()) {
+            int val1 = map1.get(key);
+            int val2 = map2.get(key);
+            if(val1 != 0 && val2 !=0) {
+                int min = Math.min(val1, val2);
+                for(int i=0; i< min;i++) {
+                    list.add(key);
+                }
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
