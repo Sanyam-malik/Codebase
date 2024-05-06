4cd840457ffc44a10a64c2cf4cd78113cccb44c0
Sam Arora <samarora1051@gmail.com>
2024-05-06 12:06:33 +0530

    MISC: Added CodeGladiators Solution Empty

---
+++ src/Contest/OptimizingCompetition.java
@@ -0,0 +1,17 @@
+package Contest;
+
+
+/*
+ * <metadata>
+ * <name>Optimizing Competition</name>
+ * <status>To Do</status>
+ * <description><img src="http://lordmaximus.duckdns.org:9000/codebase/OptimizingCompetition.png"></description>
+ * <url>https://www.techgig.com/codegladiators</url>
+ * <date>2024-05-02</date>
+ * <level>Easy</level>
+ * <notes>Self Explainary</notes>
+ * </metadata>
+ * */
+public class OptimizingCompetition {
+
+}
