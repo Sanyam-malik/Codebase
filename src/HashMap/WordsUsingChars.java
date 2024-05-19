d9d089dcff60c3ac3ab2d1f05555d6f39620087d
Sam Arora <samarora1051@gmail.com>
2024-05-19 19:13:15 +0530

    MISC: Added WordsUsingChars Placeholder

---
+++ src/HashMap/WordsUsingChars.java
@@ -0,0 +1,26 @@
+package HashMap;
+
+
+
+/*
+ * <metadata>
+ * <name>Find Words That Can Be Formed by Characters</name>
+ * <description><img src="http://lordmaximus.duckdns.org:9000/codebase/WordsUsingChars.png"></description>
+ * <status>To Do</status>
+ * <url>https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/description/</url>
+ * <date>2024-05-19</date>
+ * <level>Easy</level>
+ * <companies>Karat:Indeed:Amazon:Google</companies>
+ * <notes>Using Sets</notes>
+ * </metadata>
+ * */
+public class WordsUsingChars {
+
+    public int countCharacters(String[] words, String chars) {
+
+    }
+
+    public static void main(String args[]) {
+
+    }
+}
