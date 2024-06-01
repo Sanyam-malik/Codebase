2d141351957b56814bae2a193417cc3b0aa7128d
Sam Arora <samarora1051@gmail.com>
2024-06-02 01:50:54 +0530

    MISC: Added LengthOfLongestSubString

--- src/Arrays/MaxVowelsSubString.java
+++ src/Arrays/MaxVowelsSubString.java
@@ -9,7 +9,7 @@ import java.util.Set;
  * <description><img src="http://lordmaximus.duckdns.org:9000/codebase/Number_Vowels_SubString.png"></description>
  * <status>Completed</status>
  * <url>https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/description/</url>
- * <date>2024-03-08</date>
+ * <date>2024-06-01</date>
  * <level>Medium</level>
  * <companies>Amazon</companies>
  * <notes>Wow what a question</br></br>            Solution 1 Hint - Naive Approach Two for loops</br></br>            Solution 2 Hint - Sliding Window calculate vowels for first window then reduce the vowels in other for loop one by one</notes>
